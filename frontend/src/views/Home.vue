20c15720d8b3339916315bb7e28d6e177e8dddbb
Oliver Otchere <Oliverotchere4@gmail.com>
2021-12-12 16:54:41 +0000

    created both api and frontend with tailwindcss

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1 class="text-2xl text-gray-800 text-center font-medium">
+      Welcome The Tic Tok Built by OtchereDev
+    </h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+  
+  }
+}
+</script>
