a356e72aaed72e3887375261bf7cc9efce7a7d27
Oliver Otchere <Oliverotchere4@gmail.com>
2021-12-20 12:45:36 +0000

    added base views for suth

--- api/users/views.py
+++ api/users/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+class UserSignUpView():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserLoginView():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserForgotPassword():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserVerifyForgotPassword():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserChangePassword():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserGetProfile():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserResendActivation():
+    def post(self,request,*args,**kwargs):
+        pass
+
+class UserDeletion():
+    def delete(self,request,*args,**kwargs):
+        pass
